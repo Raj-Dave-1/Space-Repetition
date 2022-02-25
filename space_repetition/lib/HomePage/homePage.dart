aaa546b06ef4fdaadf4b75e9efee295e4f4f5dcf
Raj-Dave-1 <d21it170@charusat.edu.in>
2022-02-25 13:57:58 +0530

    Create Task
    Added AddTaskButton.dart for floatingActionButton AddTaskScreen.dart for inputing the data and also added FontAwesome Library

--- space_repetition/lib/HomePage/homePage.dart
+++ space_repetition/lib/HomePage/homePage.dart
@@ -1,6 +1,7 @@
 // 🚩 Dada Ki Jay Ho 🚩
 
 import 'package:flutter/material.dart';
+import 'AddTask/AddTaskButton.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -14,70 +15,66 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Container(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  GestureDetector(
-                    onTap: () {
-                      navigationChoice = NavigationChoice.Home;
-                      setState(() {});
-                    },
-                    child: navigationChoice == NavigationChoice.Home
-                        ? OpenChoice(Icons.home, "Home")
-                        : Icon(
-                            Icons.home,
-                            size: 26,
-                          ),
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      navigationChoice = NavigationChoice.Pomodoro;
-                      setState(() {});
-                    },
-                    child: navigationChoice == NavigationChoice.Pomodoro
-                        ? OpenChoice(Icons.timer, "Pomodoro")
-                        : Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Icon(
-                              Icons.timer,
-                              size: 26,
-                            ),
-                          ),
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      navigationChoice = NavigationChoice.Calender;
-                      setState(() {});
-                    },
-                    child: navigationChoice == NavigationChoice.Calender
-                        ? OpenChoice(Icons.calendar_today, "Calendar")
-                        : Icon(
-                            Icons.calendar_today,
-                            size: 26,
-                          ),
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      navigationChoice = NavigationChoice.Profile;
-                      setState(() {});
-                    },
-                    child: navigationChoice == NavigationChoice.Profile
-                        ? OpenChoice(Icons.person, "Profile")
-                        : Icon(
-                            Icons.person,
-                            size: 26,
-                          ),
-                  ),
-                ],
-              ),
+      floatingActionButtonLocation:
+          FloatingActionButtonLocation.miniCenterFloat,
+      floatingActionButton: AddTaskButton(),
+      bottomNavigationBar: Container(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            GestureDetector(
+              onTap: () {
+                navigationChoice = NavigationChoice.Home;
+                setState(() {});
+              },
+              child: navigationChoice == NavigationChoice.Home
+                  ? OpenChoice(Icons.home, "Home")
+                  : Icon(
+                      Icons.home,
+                      size: 26,
+                    ),
             ),
-          )
-        ],
+            GestureDetector(
+              onTap: () {
+                navigationChoice = NavigationChoice.Pomodoro;
+                setState(() {});
+              },
+              child: navigationChoice == NavigationChoice.Pomodoro
+                  ? OpenChoice(Icons.timer, "Pomodoro")
+                  : Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Icon(
+                        Icons.timer,
+                        size: 26,
+                      ),
+                    ),
+            ),
+            GestureDetector(
+              onTap: () {
+                navigationChoice = NavigationChoice.Calender;
+                setState(() {});
+              },
+              child: navigationChoice == NavigationChoice.Calender
+                  ? OpenChoice(Icons.calendar_today, "Calendar")
+                  : Icon(
+                      Icons.calendar_today,
+                      size: 26,
+                    ),
+            ),
+            GestureDetector(
+              onTap: () {
+                navigationChoice = NavigationChoice.Profile;
+                setState(() {});
+              },
+              child: navigationChoice == NavigationChoice.Profile
+                  ? OpenChoice(Icons.person, "Profile")
+                  : Icon(
+                      Icons.person,
+                      size: 26,
+                    ),
+            ),
+          ],
+        ),
       ),
     );
   }
